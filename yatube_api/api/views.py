b10d9a17d3dfc3286da6ef0c6704077030b1a085
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2022-08-15 11:06:07 +0300

    final

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post
+from posts.models import Group, Post, User
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
